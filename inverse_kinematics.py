e6c2303f7fa29f3d91d8d5d97fe8e5377d396aa2
luucccyyyy <lucyyuhe@hotmail.com>
2026-04-01 16:18:04 +0200

    Add files via upload

---
+++ inverse_kinematics.py
@@ -0,0 +1,47 @@
+import numpy as np 
+from robot_arm import Edubot
+from time import time
+from robot_arm import euler_to_rot
+robot = Edubot()
+
+
+poses = [
+    ([0.2, 0.2, 0.2], [0.000, 1.570, 0.650]),
+    ([0.2, 0.1, 0.4], [0.000, 0.000, -1.570]),
+    ([0.0, 0.0, 0.4], [0.000, -0.785, 1.570]),
+    ([0.0, 0.0, 0.07], [3.141, 0.000, 0.000]),
+    ([0.0, 0.0452, 0.45], [-0.785, 0.000, 3.141])
+]
+
+initial_guesses = [
+    [0, 0, 0, 0, 0],
+    [0, 0.5, -0.5, 0, 0],
+    [-1, 0.5, 0.5, -0.5, 0],
+    [1, -0.5, 0.5, 0.5, 0]
+]
+
+t_start = time()
+for i, (pos, rot) in enumerate(poses):
+
+
+    R = euler_to_rot(*rot)
+    q, error = robot.inverse_kinematics_optimization(pos, R)
+
+    print(f"\nPose {i+1}")
+    print("q:", q)
+    print("error:", error)
+
+    solutions = []
+
+    for guess in initial_guesses:
+        q, error = robot.inverse_kinematics_optimization(pos, R, initial_guess=guess)
+        if error < 0.02:  
+            solutions.append(q)
+
+    print(f"\nPose {i + 1}: {len(solutions)} feasible solutions found")
+    for idx, q_sol in enumerate(solutions):
+        print(f"Solution {idx + 1}: {q_sol}")
+
+t_end = time()
+
+print("Time Elapsed For Optimization: ", t_end - t_start)
